4e8b93c561842d80d3e0274c51b650939dd09ebb
syahril343 <syahril.21058@mhs.unesa.ac.id>
2025-09-05 02:41:19 +0700

    &&&

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
+
+// pages
+import Home from "./pages/Home";
+
 const App = () => {
   return (
-    <div className="bg-accent text-primary text-3xl font-bold flex items-center gap-2 p-4">
-      <i
-        className="fa-solid fa-cog animate-bounce duration-500"
-       
-      ></i>
-      <i className="fa-solid fa-heart animate-bounce duration-500"></i>
-      Ini Undangan Pertama
-    </div>
+    <>
+    <Home />
+    </>
   );
 };
 
---
+++ src/pages/Akad.tsx
@@ -0,0 +1,10 @@
+
+const Akad = () => {
+  return (
+    <div>
+      Ini halaman Jadwal Akad Nikah
+    </div>
+  )
+}
+
+export default Akad
