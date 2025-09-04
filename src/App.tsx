4779c0555bf647efd1c71ec4fc53ccd642bf061e
syahril343 <syahril.21058@mhs.unesa.ac.id>
2025-09-05 02:24:51 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="bg-accent text-primary text-3xl font-bold flex items-center gap-2 p-4">
+      <i
+        className="fa-solid fa-cog animate-bounce duration-500"
+       
+      ></i>
+      <i className="fa-solid fa-heart animate-bounce duration-500"></i>
+      Ini Undangan Pertama
+    </div>
+  );
+};
+
+export default App;
