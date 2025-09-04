68398135f97ba5c5ec649a8cc04cea1a8157fc58
syahril343 <syahril.21058@mhs.unesa.ac.id>
2025-09-05 02:34:04 +0700

    Halaman Baru

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div>
+      Ini halaman Home
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/RSVP.tsx
@@ -0,0 +1,11 @@
+
+
+const RSVP = () => {
+  return (
+    <div>
+      Ini RSVP
+    </div>
+  )
+}
+
+export default RSVP
